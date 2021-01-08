0f8bf6d12df2ab0a712cc9d986ee8b94d9a0a404
jloesch30 <61180397+jloesch30@users.noreply.github.com>
2021-01-08 12:25:54 -0600

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/production-sub-path/'
-        : '/'
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/personal-website/'
+    : '/'
 };
