70c3dd8c419180f8485a0a75d7d3facc74cb70fb
jloesch30 <61180397+jloesch30@users.noreply.github.com>
2021-01-14 13:43:18 -0600

    about page parallax

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,23 +1,65 @@
 <template>
   <div>
-    This is the about page
+    <the-nav></the-nav>
   </div>
-  <!-- bootstrap fixed class -->
+  <div class="parallax bg-image-1"></div>
+  <div class="container">
+    <div class="content">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus
+        laborum amet minima eos accusamus quos. Doloribus ratione incidunt
+        exercitationem necessitatibus atque, dignissimos voluptatum sunt, unde
+        dicta enim a impedit soluta.
+      </p>
+    </div>
+  </div>
+
+  <div class="parallax bg-image-2"></div>
+  <div class="container">
+    <div class="content">
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugiat quam
+        dolor minima vitae libero veniam laboriosam ducimus corrupti molestiae
+        praesentium? Facilis aut eaque quia. Ad sunt harum incidunt modi! Sint!
+      </p>
+    </div>
+  </div>
+
   <div>
     <the-contact></the-contact>
   </div>
 </template>
 
 <script>
-import TheContact from '../components/TheContact.vue';
-
+import TheContact from "../components/TheContact.vue";
+import TheNav from "@/components/TheNav.vue";
 export default {
+  data() {
+    return {};
+  },
+  computed: {},
   components: {
-    TheContact
-  }
+    TheContact,
+    TheNav,
+  },
 };
 </script>
 
-<style>
+<style scoped>
+.parallax {
+  /* Set a specific height */
+  height: 500px;
 
+  /* Create the parallax scrolling effect */
+  background-attachment: fixed;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.bg-image-1 {
+  background-image: url("../assets/imgs/camera.jpg");
+}
+.bg-image-2 {
+  background-image: url("../assets/imgs/friends.jpg");
+}
 </style>
