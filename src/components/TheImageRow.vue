f6fa5b5a5e5a8d4e3a36220d76a9cc6560a355fd
jloesch30 <61180397+jloesch30@users.noreply.github.com>
2021-01-19 00:21:08 -0600

    no big changes

---
+++ src/components/TheImageRow.vue
@@ -0,0 +1,29 @@
+<template>
+  <section>
+    <div class="row">
+      <div class="col l3 ">
+      <p>
+        Hello there
+      </p>
+      </div>
+      <div class="col l3 "></div>
+      <div class="col l3 "></div>
+      <div class="col l3 "></div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    images: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -5,7 +5,7 @@
   <section class="container">
     <div class="seperator"></div>
     <div class="row">
-      <h4 class="header">About Josh</h4>
+      <h4 class="header">About Me</h4>
     </div>
   </section>
   <section class="container">
@@ -25,25 +25,55 @@
     <div class="seperator"></div>
     <div class="row container">
       <div class="col l12 center">
-        <img src="../assets/imgs/mccombs.png" alt="ut-austin">
+        <img src="../assets/imgs/mccombs.png" alt="ut-austin" />
+      </div>
+      <div class="content">
+        <p id="paragraph">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim esse
+          impedit aliquid omnis quidem quod, maiores dolor modi ex eius, harum
+          architecto laboriosam distinctio doloremque necessitatibus itaque?
+          Quisquam, ea magnam?Lorem ipsum dolor sit amet consectetur adipisicing
+          elit. Enim esse impedit aliquid omnis quidem quod, maiores dolor modi
+          ex eius, harum architecto laboriosam distinctio doloremque
+          necessitatibus itaque? Quisquam, ea magnam?Lorem ipsum dolor sit amet
+          consectetur adipisicing elit. Enim esse impedit aliquid omnis quidem
+          quod, maiores dolor modi ex eius, harum architecto laboriosam
+          distinctio doloremque necessitatibus itaque? Quisquam, ea magnam?
+        </p>
       </div>
     </div>
   </section>
+  <section>
+    <div class="seperator"></div>
+    <the-image-row
+    :images="images"/>
+  </section>
 </template>
-
 <script>
 import M from "materialize-css";
 import TheNav from "@/components/TheNav.vue";
+import TheImageRow from "@/components/TheImageRow.vue";
 export default {
   data() {
     return {};
   },
   components: {
     TheNav,
+    TheImageRow
   },
   mounted() {
     M.AutoInit();
   },
+  computed: {
+    images() {
+      return {
+        i1: "friends.jpg",
+        i2: "milk.jpg",
+        i3: "lake-1.jpg",
+        i4: "milk-1.jpg"
+      };
+    }
+  }
 };
 </script>
 
@@ -51,6 +81,10 @@ export default {
 img {
   width: 80%;
   height: auto;
+}
+.carousel-item {
+  height: 32rem;
+  background: #777;
 }
 .seperator {
   margin: 4rem;
